957edcb8acd928610d98be0c1bac072e8310897e
Vladislav Koroteev <koroteika@icloud.ru>
2024-11-04 00:01:39 +0300

    Adding code to perform addition in C++

--- main.cpp
+++ main.cpp
@@ -1,1 +1,14 @@
-﻿
+﻿#include<iostream>
+using namespace std;
+
+int main()
+{
+	int a, b;
+	cout << "Введите первое число: ";
+	cin >> a;
+	cout << "Введите второе число: ";
+	cin >> b;
+
+	cout << "Сумма (сложение) чисел = " << (a + b) << endl;
+	return 0;
+}
